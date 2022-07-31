e06e30a07695bec5ce0b33e93ab63a2986a39535
Parimibhargavi <106258260+Parimibhargavi@users.noreply.github.com>
2022-07-31 14:55:04 +0530

    Add files via upload

---
+++ A.js
@@ -0,0 +1,18 @@
+[2:03 pm, 31/07/2022] Indhu💜: <!DOCTYPE html>
+<html>
+
+<body>
+
+<script>
+var num
+num = prompt('Enter the number')
+if (num % 3 == 0)
+document. write('talentio')
+else(num % 5 == 0)
+document. write('company')
+elsif(num % 3 == 0 | num % 5 == 0)
+doctment. write('talentio company')
+</script>
+</body>
+
+</html>
